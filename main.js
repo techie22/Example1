2f19c8b65dec6532d7e7ad97d703bf4243bc761a
techie22 <tech.heena@gmail.com>
2021-02-19 20:09:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function changeName(){
+    document.getElementById("heading").innerHTML="Welcome to my Webpage!!!";
+    document.getElementById("heading").style.backgroundColor="lawngreen";
+    document.getElementById("heading").style.color="black";
+}
